c915cd272363142c9411386323ef5bb321920c26
Shardul Bhatt <shardul.bhatt@gmail.com>
2011-12-01 18:58:35 +0530

    First commit.

---
+++ examples/ex_add_commit.rb
@@ -0,0 +1,16 @@
+require '../lib/grit'
+
+count = 1
+Dir.chdir("/Users/schacon/projects/atest") do
+  r = Grit::Repo.new('.')
+  while(count < 10) do
+    fname = Time.now.to_i.to_s + count.to_s
+    File.open(fname, 'w') { |f| f.write('hellor ' + fname) }
+    r.add(fname)
+    count += 1
+  end
+  r.commit_index('my commit')
+
+  #Shardul#This is a comment to comment just to induce a change in this file.
+	
+end
