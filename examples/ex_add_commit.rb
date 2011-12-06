2e797362b9cbd539e139fec46416154a9ea65f17
Shardul Bhatt <shardul.bhatt@gmail.com>
2011-12-06 12:14:58 +0530

    Made changes in code.

--- examples/ex_add_commit.rb
+++ examples/ex_add_commit.rb
@@ -6,8 +6,9 @@ Dir.chdir("/Users/schacon/projects/atest") do
   while(count < 10) do
     fname = Time.now.to_i.to_s + count.to_s
     File.open(fname, 'w') { |f| f.write('hellor ' + fname) }
+    #r.add(fname)
+    #count += 1
     r.add(fname)
-    count += 1
   end
   r.commit_index('my commit')
 
